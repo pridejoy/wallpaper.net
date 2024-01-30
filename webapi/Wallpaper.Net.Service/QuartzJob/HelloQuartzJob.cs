4289fda5b5c9981b11253bd5b66ee1526fb8ab78
pridejoy <pridejoy@foxmail.com>
2024-01-30 17:03:04 +0800

    1

--- webapi/Wallpaper.Net.Service/QuartzJob/HelloQuartzJob.cs
+++ webapi/Wallpaper.Net.Service/QuartzJob/HelloQuartzJob.cs
@@ -1,39 +1,35 @@
 ﻿using Newtonsoft.Json;
 using Quartz;
 using SqlSugar;
-using Wallpaper.Net.Service.Entity; 
+using Wallpaper.Net.Service;
+using Wallpaper.Net.Service.Entity;
 
-namespace Wallpaper.Net.Service.QuartzNET
+public class HelloQuartzJob : IJob
 {
-    public class HelloQuartzJob : IJob
+    /// <summary>
+    /// 当前任务执行的Core表达式
+    /// </summary>
+    public static string Cron = "*/1 * * * * ?";
+
+
+    public async Task Execute(IJobExecutionContext context)
     {
-        /// <summary>
-        /// 当前任务执行的Core表达式
-        /// </summary>
-        public static string Cron = "*/1 * * * * ?"; 
-
- 
-        public async Task Execute(IJobExecutionContext context)
+        try
         {
-            try
-            {
-                //业务处理逻辑
-                Console.WriteLine("111111111111111");
+            //业务处理逻辑
+            Console.WriteLine("111111111111111");
 
-                // 查询数据库表
-                var date = await Sqlsugar.db.Queryable<bs_gallery>()
-                    .Where(x => x.IsDelete == false)
-                    .OrderBy(x => SqlFunc.GetRandom())
-                    .FirstAsync();
+            // 查询数据库表
+            var date = await Sqlsugar.db.Queryable<bs_gallery>()
+                .Where(x => x.IsDelete == false)
+                .OrderBy(x => SqlFunc.GetRandom())
+                .FirstAsync();
 
-                Console.WriteLine(JsonConvert.SerializeObject(date));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-                FileHelper.Write(ex.Message,"error");
-            }
+            Console.WriteLine(JsonConvert.SerializeObject(date));
+        }
+        catch (Exception ex)
+        {
+            FileHelper.Write(ex.Message, "error");
         }
     }
-
 }
---
+++ webapi/Wallpaper.Net.Service/Service/Sqlsugar.cs
@@ -0,0 +1,12 @@
+﻿using SqlSugar;
+
+public static class Sqlsugar
+{
+    //创建数据库对象
+    public static SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+    {
+        ConnectionString = File.ReadAllText("D:\\db.txt"),
+        DbType = DbType.SqlServer,
+        IsAutoCloseConnection = true
+    });
+}
